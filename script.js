469659c57fa5d8b39149b043616a74d38ed401a1
Pallavi-02-khandate <pkhandate2002@gmail.com>
2026-01-30 11:32:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function getFormvalue() {
     //Write your code here
 
+	let first=document.getElementByTagName('input')[0]
+	let last=document.getElementByTagName('input')[1]
+	let btn=document.getElementById('input')[2]
+
+	let fullName =`${first.value} ${last.value}`
+
+		
+
+	btn.addEventListener("click", function(){
+		
+		alert(fullName)
+	})
+
+	
 }
